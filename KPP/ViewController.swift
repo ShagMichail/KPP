35d523c1b5233c6fa352be502c5050317fcfb8db
ShagMichail <shiga2001@mail.ru>
2022-02-24 18:09:23 +0300

    Initial Commit

---
+++ KPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KPP
+//
+//  Created by Михаил Шаговитов on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
